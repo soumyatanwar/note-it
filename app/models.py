731b36821f1d3e4175059a2e20a81036ae386d5d
Soumya Tanwar <soumyatanwar23@gmail.com>
2021-04-17 13:04:29 +0530

    Created a Notes Model

--- app/models.py
+++ app/models.py
@@ -4,3 +4,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import logging
 
 db = SQLAlchemy(app)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120), unique=True, nullable=False)
+    created_on = db.Column(db.Date(), nullable=False)
+    # _is_done
+    # _is_deleted
+    # created_user_id
+    # shared_with_user_id
