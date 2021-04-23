a19795c4a586a78b263338e84d208ddf5ac11d1d
Soumya Tanwar <soumyatanwar23@gmail.com>
2021-04-24 01:31:55 +0530

    Added user, user profile models and linked to note

--- app/models.py
+++ app/models.py
@@ -13,3 +13,21 @@ class Note(db.Model):
     _is_done = db.Column(db.Boolean, nullable=False)
     _is_deleted = db.Column(db.Boolean, nullable=False)
     note_image = db.Column(db.LargeBinary, nullable=False)
+    created_user = db.Column(db.Integer, db.ForeignKey('user.username'), nullable=False)
+    user = db.relationship('User', backref=db.backref('users', lazy=True))
+    
+class User(db.Model):
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(8), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class UserProfile(db.Model):
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    bio = db.Column(db.Text, unique=False, nullable=True)
+    username = db.Column(db.Integer, db.ForeignKey('user.username'), nullable=False)
+    user = db.relationship('User', backref=db.backref('users', lazy=True))
+
+    def __repr__(self):
+        return '<Profile %r>' % self.bio
