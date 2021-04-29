7b9185ad1ce15985f95b3d174f6f5f0e20649d55
Soumya Tanwar <soumyatanwar23@gmail.com>
2021-04-29 21:16:16 +0530

    Initialized db before running app, so it is registered and available for the app to use

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, request
 from dotenv import load_dotenv 
 import os
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 db_url = os.environ["DATABASE_URL"]
@@ -11,6 +12,8 @@ print(db_url)
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
 
 import models
 
@@ -37,4 +40,5 @@ def show_notes():
 
 # Run the app in port 5000 and in debug mode
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
