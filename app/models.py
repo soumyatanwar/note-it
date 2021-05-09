e7946dcc0c157c4d7f7f10fdf1bc664ed5ffbc55
Soumya Tanwar <soumyatanwar23@gmail.com>
2021-05-09 14:24:55 +0530

    Added user reference to note, combined user & user profile entities

--- app/models.py
+++ app/models.py
@@ -13,24 +13,17 @@ class Note(db.Model):
     _is_done = db.Column(db.Boolean, nullable=False, default = False)
     _is_deleted = db.Column(db.Boolean, nullable=False, default = False)
     note_image = db.Column(db.LargeBinary, nullable=True)
-    # created_user = db.Column(db.String(80), db.ForeignKey('user.username'), nullable=False)
-    # user = db.relationship('User', backref=db.backref('users', lazy=True))
+    created_user = db.Column(db.String(80), db.ForeignKey('user.username'), nullable=False)
+    user = db.relationship('User', backref=db.backref('users', lazy=True))
     
 class User(db.Model):
     username = db.Column(db.String(80), primary_key=True, nullable=False)
     password = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    bio = db.Column(db.Text, unique=False, nullable=True)
+    friendly_name = db.Column(db.String(20), unique=False, nullable=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
-
-class UserProfile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    bio = db.Column(db.Text, unique=False, nullable=True)
-    username = db.Column(db.String(80), db.ForeignKey('user.username'), nullable=False)
-    user = db.relationship('User', backref=db.backref('user', lazy=True))
-
-    def __repr__(self):
-        return '<Profile %r>' % self.bio
 
 # db.create_all() # should only create db once
